d6db6a6483633e2c249c0090f40f6ac5a843758a
leilycoquard <leilycoquard@gmail.com>
2022-08-08 16:24:08 +0200

    desktop cards

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function showBoxMenu(newMenuBtn){
         }
     });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
